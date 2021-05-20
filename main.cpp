c0d5cb7cc5adcfd88512a4b47a46b50cb657724f
Jeżozwierz <filippatrykpietryga@gmail.com>
2021-05-20 19:57:51 +0200

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "md5.h"
+
+int main()
+{
+	//Md5 md5 = new Md5();
+	//char* result = md5(1);
+}
---
+++ md5.h
@@ -0,0 +1,11 @@
+#pragma once
+class Md5
+{
+public:
+	Md5();
+	Md5(const Md5&);
+	Md5& operator=(const Md5&);
+	int s[64], k[64];
+	int i;
+	char* calc(char *);
+};
